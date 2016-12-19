d49f28f5fc93c732ade14ad756e674bd65a6da4c
jomi2003 <jose.quinchuela@gmail.com>
2016-12-19 13:18:28 -0500

    RMF -Tool code

--- Phase13.Web/Controllers/HomeController.cs
+++ Phase13.Web/Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace Phase13.Web.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "The RMF Tool";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "SEC RMF Tool - Headquarters";
 
             return View();
         }
